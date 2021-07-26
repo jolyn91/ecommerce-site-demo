6a1379de0fd657feb2461636e6af6c6f6a505ec1
Jolyn Foo <joyofoln@gmail.com>
2021-07-27 03:35:48 +0800

    Update url

--- src/settings.ts
+++ src/settings.ts
@@ -12,7 +12,7 @@ export enum ReactEnv {
 
 switch (process.env.NODE_ENV) {
     case ReactEnv.DEV:
-        API_SERVER_VAL = `${proxy}/https://api.ablr.com`;
+        API_SERVER_VAL = `${proxy}/https://api.uat.ablr.com`;
         break;
     case ReactEnv.PROD:
         API_SERVER_VAL = `${proxy}/https://api.uat.ablr.com`;
